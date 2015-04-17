78bdefd629e4435022f3ad646f158871f13be0db
Dana Scheider <dana.scheider@gmail.com>
2015-04-17 15:10:16 -0700

    Add initial tests for Barista.Model functionality

--- spec/baristaSpec.js
+++ spec/baristaSpec.js
@@ -8,4 +8,29 @@ describe('Barista', function() {
   it('has a Collection property', function() {
     expect(typeof Barista.Collection).not.toBe('undefined');  
   });
+
+  describe('Barista.Model', function() {
+    describe('isA', function() {
+      it('returns true with arg \'Backbone.Model\'', function() {
+        var newModel = new Barista.Model();
+        expect(newModel.isA('Backbone.Model')).toBe(true);
+      });
+
+      it('returns true with arg\'Barista.Model\'', function() {
+        var newModel = new Barista.Model();
+        expect(newModel.isA('Barista.Model')).toBe(true);
+      });
+    });
+  });
+
+  describe('config', function() {
+
+    // The config method takes an app object as input and extends Barista.Model
+    // and Barista.Collection using the models and collections defined on the 
+    // app object.
+
+    it('creates a Barista.TaskModel object', function() {
+      //
+    });
+  });
 });
