3d7870646872fb71f5c14ff22483c464ed05666e
Dana Scheider <dana.scheider@gmail.com>
2015-04-20 13:24:20 -0700

    Tests for the sync method

--- spec/baristaCollectionSpec.js
+++ spec/baristaCollectionSpec.js
@@ -1,8 +1,11 @@
 var Barista    = require(process.cwd() + '/lib/barista.js'),
     ExampleApp = require(process.cwd() + '/spec/ex1/exampleApp1.js'),
+    Backbone   = require('backbone'),
     context    = describe;
 
 describe('Barista.Collection', function() {
+  var collection, spy;
+
   describe('properties', function() {
     it('has model Barista.Model by default', function() {
       expect(Barista.Collection.prototype.model).toBe(Barista.Model);
@@ -23,6 +26,22 @@ describe('Barista.Collection', function() {
     it('returns false with another argument', function() {
       var newCollection = new Barista.Collection();
       expect(newCollection.isA('Porsche')).toBe(false);
+    });
+  });
+
+  describe('sync', function() {
+    beforeEach(function() {
+      spyOn(Backbone, 'sync');
+      collection = new Barista.Collection();
+    });
+
+    it('doesn\'t call Backbone.sync', function() {
+      collection.sync();
+      expect(Backbone.sync).not.toHaveBeenCalled();
+    });
+
+    it('returns the collection', function() {
+      expect(collection.sync()).toBe(collection);
     });
   });
 });
