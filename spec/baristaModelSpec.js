5692229fb6a8240bb6bf40c63e1369e18d136591
Dana Scheider <dana.scheider@gmail.com>
2015-04-20 10:49:38 -0700

    Add specs for the sync method of the model prototype

--- spec/baristaModelSpec.js
+++ spec/baristaModelSpec.js
@@ -1,8 +1,15 @@
 var Barista    = require(process.cwd() + '/lib/barista.js'),
+    Backbone   = require('backbone'),
     ExampleApp = require(process.cwd() + '/spec/ex1/exampleApp1.js'),
     context    = describe;
 
 describe('Barista.Model', function() {
+  var model;
+
+  beforeEach(function() {
+    Barista.config(ExampleApp);
+  });
+
   describe('isA', function() {
     it('returns true with arg \'Backbone.Model\'', function() {
       var newModel = new Barista.Model();
@@ -20,21 +27,19 @@ describe('Barista.Model', function() {
     });
   });
 
-  describe('prototype', function() {
-    describe('initialize', function() {
-      it('exists', function() {
-        expect(typeof Barista.Model.prototype.initialize).toBe('function');
-      });
+  describe('sync', function() {
+    beforeEach(function() {
+      model = new Barista.TaskModel();
+      spyOn(Backbone, 'sync');
     });
 
-    describe('changed', function() {
-      it('is defined', function() {
-        expect(typeof Barista.Model.prototype.changed).toBe('object');
-      });
-
-      it('is null', function() {
-        expect(Barista.Model.prototype.changed).toBe(null);
-      });
+    it('does not call Backbone.sync', function() {
+      model.sync();
+      expect(Backbone.sync).not.toHaveBeenCalled();
     });
+  });
+
+  describe('get', function() {
+    
   });
 });
